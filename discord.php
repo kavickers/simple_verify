d24ef338a690af131f62e1226b742305021e2533
minecraftrules96 <kykyanthony@gmail.com>
2017-12-30 07:08:14 -0600

    Update discord.php

--- discord.php
+++ discord.php
@@ -23,10 +23,8 @@ $token = bin2hex(mcrypt_create_iv(22, MCRYPT_DEV_URANDOM));
 $generate = $_POST['generate']; //this corresponds with the name of the button input => [<input class="blue-submit" type="submit" name="generate" value="Generate Token">] <= note the name is equal to generate
 
 if ($generate) {
-	$gentoken = $handler->prepare("UPDATE users SET discordToken = :discord WHERE id = :id"); // again, same here, edit this accordingly to how you retrieve variables
-	$gentoken->bindParam(':discord', $token);
-	$gentoken->bindParam(':id', $myU->id); // again, same here, please edit accordingly to how you retrieve user variables [like the id]
-	$gentoken->execute();
+	$gentoken = $handler->prepare("UPDATE users SET discordToken = ? WHERE id = ?"); // again, same here, edit this accordingly to how you retrieve variables
+	$gentoken->execute(array($token, $myU->id)); // again, same here, please edit accordingly to how you retrieve user variables [like the id]
 	header('Location: https://www.yourwebsite.com/discord'); // you can have it located in any type of file hierchy, just please edit it here!
 }
 
